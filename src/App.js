d3de6e61a915a91bbeb3b56c8fb8ca09266e6561
Gabriele <gabriieele6@gmail.com>
2022-05-14 10:09:29 -0300

    feat: set calculator structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
 
 function App() {
-    return <h1>Hello, world!</h1>;
+    return (
+        <div className="App">
+            <div className="calculator">
+                <div className="display">
+                    <span>()</span>
+                </div>
+                <div className="operators">
+                    <button>+</button>
+                    <button>-</button>
+                    <button>x</button>
+                    <button>/</button>
+                    <button>DEL</button>
+                    <button>CE</button>
+                </div>
+                <div className="digits">
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9 </button>
+                    <button>0</button>
+                    <button>.</button>
+                    <button>=</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
