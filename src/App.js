c70d6062c20267c11345e9ff460a320c0a44b7ab
Gabriele <gabriieele6@gmail.com>
2022-05-15 22:28:40 -0300

    feat: update the calculation with buttons' values

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+    const [calc, setCalc] = useState("");
+    const [result, setResult] = useState("");
+
+    const operators = ["/", "*", "+", "-", "."];
+
+    const updateCalc = (value) => {
+        if (
+            (operators.includes(value) && calc === "") ||
+            (operators.includes(value) && operators.includes(calc.slice(-1)))
+        ) {
+            return;
+        }
+
+        setCalc(calc + value);
+
+        if (!operators.includes(value)) {
+            setResult(eval(calc + value).toString());
+        }
+    };
     return (
         <div className="App">
             <div className="calculator">
                 <div className="display">
-                    <span>()</span>
+                    {result ? <span>({result})</span> : ""}
+                    {calc || "|"}
                 </div>
                 <div className="operators">
-                    <button>+</button>
-                    <button>-</button>
-                    <button>x</button>
-                    <button>/</button>
+                    <button onClick={() => updateCalc("+")}>+</button>
+                    <button onClick={() => updateCalc("-")}>-</button>
+                    <button onClick={() => updateCalc("*")}>x</button>
+                    <button onClick={() => updateCalc("/")}>/</button>
                     <button>DEL</button>
                     <button>CE</button>
                 </div>
                 <div className="digits">
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9 </button>
-                    <button>0</button>
-                    <button>.</button>
+                    <button onClick={() => updateCalc("1")}>1</button>
+                    <button onClick={() => updateCalc("2")}>2</button>
+                    <button onClick={() => updateCalc("3")}>3</button>
+                    <button onClick={() => updateCalc("4")}>4</button>
+                    <button onClick={() => updateCalc("5")}>5</button>
+                    <button onClick={() => updateCalc("6")}>6</button>
+                    <button onClick={() => updateCalc("7")}>7</button>
+                    <button onClick={() => updateCalc("8")}>8</button>
+                    <button onClick={() => updateCalc("9")}>9 </button>
+                    <button onClick={() => updateCalc("0")}>0</button>
+                    <button onClick={() => updateCalc(".")}>.</button>
                     <button>=</button>
                 </div>
             </div>
